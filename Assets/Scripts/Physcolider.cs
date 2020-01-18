b4ba68601e976d6f8328c895387be536e12d0d5e
Thomas Haller <Thomas.Haller@lab10.coop>
2020-01-18 21:31:51 +0100

    Hit something untagged...

---
+++ Assets/Scripts/Physcolider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physcolider : MonoBehaviour
+{
+    private float currentSpeed;
+    private float collisionSpeed;
+    private bool bumped;
+
+    private Rigidbody _body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = this.GetComponentInParent<Rigidbody>();
+        Debug.Log("collider init.");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Player hit!!");
+            if (currentSpeed > 3)
+            {
+                collisionSpeed = currentSpeed;
+                bumped = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Hit something else " + collision.collider.gameObject.tag);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        currentSpeed = _body.velocity.magnitude;
+
+        if (bumped)
+        {
+            //health -= collisionSpeed - currentSpeed;
+            Debug.Log("Player Pumped!!");
+
+            bumped = false;
+        }
+    }
+}
